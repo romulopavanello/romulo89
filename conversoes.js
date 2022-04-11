adbef3785ae8d239e5c866ccfbebaaf00a759f98
romulopavanello <romulo.pavanello@gmail.com>
2022-04-11 09:38:15 -0300

    codigo var e coversoes

---
+++ conversoes.js
@@ -0,0 +1,12 @@
+// tipo de dado
+// booleanos
+
+//conversao implicita
+const numero= 456;
+const numerostring = Number('456');
+
+//Number()
+//String()
+console.log(numero + numerostring)
+
+// conversão explicita
